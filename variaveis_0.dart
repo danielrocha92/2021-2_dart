b48bbfe0729859d41fe799876767572fe25bd077
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 13:03:16 -0300

    Update variaveis_0.dart

--- variaveis_0.dart
+++ variaveis_0.dart
@@ -1,33 +1,33 @@
-void  main () {
+void main() {
 
-  // String -> tipo texto
-  String nomeCurso =  "Desenvolvimento Móvel" ;
+  //String -> tipo texto
+  String nomeCurso = "Desenvolvimento Móvel";
 
-  // int -> tipo números reais, inteiros
-  int qtdAlunos =  100 ;
+  //int -> tipo números reais, inteiros
+  int qtdAlunos = 100;
 
-  // double -> tipo numero decimal, de ponto flutuante
-  duplo mediaGeral =  9,5 ;
+  //double -> tipo numero decimal, de ponto flutuante
+  double mediaGeral = 9.5;
 
-  // bool -> Tipo binário, verdadeiro ou falso
-  bool temVaga =  true ;
+  //bool -> Tipo binário, verdadeiro ou falso
+  bool temVaga = true;
 
-  imprimir (nomeCurso); // imprime o nome do nome do curso
+  print(nomeCurso); //imprime o nome do nome do curso
 
-  // impressão de um texto concatenado com uma variável:
-  print ( "A Uninove oferece à você o curso de"  + nomeCurso);
+  //impressão de um texto concatenado com uma variável:
+  print("A Uninove oferece à você o curso de " + nomeCurso);
 
-  // interpolação de uma variável com o texto (muito usado):
-  print ( "O curso $ nomeCurso tem $ qtdAlunos alunos!: D" );
-  print ( "Os discentes tiram a nota $ mediaGeral em média!" );
+  //interpolação de uma variável com o texto (muito usado):
+  print("O curso $nomeCurso tem $qtdAlunos alunos! :D");
+  print("Os discentes tiram a nota $mediaGeral em média!");
 
-  // impressão de variáveis ​​booleanas:
-  imprimir ( "Este curso tem vagas? $ temVaga " );
+  //impressão de variáveis booleanas:
+  print("Este curso tem vagas? $temVaga");
 
-  // o tipo dinâmico permite qualquer valor.
-  // ele assume o valor do último tipo:
-  dinâmica variavelDinamica =  1 ;
-  imprimir (variavelDinamica);
-  variavelDinamica =  "teste" ;
-  imprimir (variavelDinamica);
+  //o tipo dynamic permite qualquer valor.
+  //ele assume o valor do último tipo:
+  dynamic variavelDinamica = 1;
+  print(variavelDinamica);
+  variavelDinamica = "teste";
+  print(variavelDinamica);
 }
