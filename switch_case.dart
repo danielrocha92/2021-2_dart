7857dfb49310862d18d69c8fb52f6bccc2714aba
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 12:57:20 -0300

    Create switch_case.dart

---
+++ switch_case.dart
@@ -0,0 +1,23 @@
+void  main () {
+
+  String tipoDeRoupa =  "Camisetas" ;
+
+  // seleção de um programa de máquina de lavar
+  switch (tipoDeRoupa) {
+    estojo  "Jeans" :
+      imprimir ( "Lavagem pesada" );
+      pausa ;
+    case  "Seda" :
+      imprimir ( "Lavagem delicada" );
+      pausa ;
+    estojo  "Pano de chão" :
+      imprimir ( "Lavagem pesada com alvejante" );
+      pausa ;
+    case  "Camisetas" :
+      imprimir ( "Lavagem diária" );
+      pausa ;
+    padrão :
+      imprimir ( "verificar uma etuqueta" );
+      pausa ;
+  }
+}
