0ebd3b6ac2ef6f4f4aa63f8e904a03d756758fbf
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 13:34:27 -0300

    Update variaveis.dart

--- variaveis.dart
+++ variaveis.dart
@@ -1,32 +1,32 @@
-void  main () {
-  // variáveis
+void main() {
+  //variáveis
   bool aprovado;
-  double nota_av1 =  10,0 ;
-  double nota_av2 =  9,5 ;
+  double nota_av1 = 10.0;
+  double nota_av2 = 9.5;
 
-  // cálculo da média
-  mídia dupla = (nota_av1 + nota_av2) /  2 ;
+  //cálculo da média
+  double media = (nota_av1 + nota_av2) / 2;
 
-  // se média é menor que 6:
-  if (media <  6 ) {
-    imprimir ( "Infelizmente não passou! :(" );
-    aprovado =  falso ;
-  } else  if (media ! =  10 ) {
-    // se a média é diferente de dez
-    imprimir ( "Passou! :)" );
-    aprovado =  verdadeiro ;
+  //se média é menor que 6:
+  if (media < 6) {
+    print("Infelizmente não passou! :(");
+    aprovado = false;
+  } else if (media != 10) {
+    //se a média é diferente de dez
+    print("Passou! :)");
+    aprovado = true;
   } else {
-    // se a média é igual a dez
-    imprimir ( "Topo!: D" );
-    aprovado =  verdadeiro ;
+    //se a média é igual a dez
+    print("Top! :D");
+    aprovado = true;
   }
 
-  // se ternário em DART.
-  // repare que uma variável "info" recece o retorno da condição
-  // Sintaxe faça se ternário:
-  // <condição>? [VALOR SE VERDADEIROS]: [VALOR SE FALSO]
-  String info = aprovado ?  "Aprovado!"  :  "Reprovado" ;
+  //if ternário em DART.
+  //repare que a variável "info" recece o retorno da condição
+  //Sintaxe do if ternário:
+  // <condição> ? [VALOR SE VERDADEIROS] : [VALOR SE FALSO]
+  String info = aprovado ? "Aprovado!" : "Reprovado";
 
-  // imprime uma variável "info":
-  imprimir (informações);
+  //imprime a variável "info":
+  print(info);
 }
