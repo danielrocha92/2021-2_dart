fc1753f3a67cc1ab34530282f4e475859d907abe
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 13:21:18 -0300

    Update maps.dart

--- maps.dart
+++ maps.dart
@@ -1,22 +1,22 @@
-class  Veiculo {
+class Veiculo {
   String fabricante;
   String modelo;
   int ano;
 
-  Veiculo ( este .fabricante, este .modelo, este .ano);
+  Veiculo(this.fabricante, this.modelo, this.ano);
 }
 
-void  main () {
+void main() {
 
-  Map < String , Veiculo > veiculos =  Map ();
+  Map<String, Veiculo> veiculos = Map();
 
-  veiculos [ "ABC-123" ] =  Veiculo ( "VW" , "Passat" , 2020 );
-  veiculos [ "ABC-321" ] =  Veiculo ( "Audi" , "A8" , 2021 );
-  veiculos [ "BBB-1234" ] =  Veiculo ( "Ferrari" , "F-250" , 2018 );
+  veiculos["ABC-123"] = Veiculo("VW", "Passat", 2020);
+  veiculos["ABC-321"] = Veiculo("Audi", "A8", 2021);
+  veiculos["BBB-1234"] = Veiculo("Ferrari", "F-250", 2018);
 
 
   //print(veiculos["ABC-321"].modelo);
 
-  veiculos. forEach ((k, v) =>  print ( "Placa: $ { k } , $ { v . fabricante } - $ { v . modelo } " ));
+  veiculos.forEach((k, v) => print("Placa: ${k}, ${v.fabricante} - ${v.modelo}"));
 
 }
