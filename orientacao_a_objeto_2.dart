c4a952246dff7862a0f09c392a5443202452c735
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 13:25:47 -0300

    Update orientacao_a_objeto_2.dart

--- orientacao_a_objeto_2.dart
+++ orientacao_a_objeto_2.dart
@@ -1,71 +1,71 @@
-classe  Botao {
+class Botao {
   String cor;
   String texto;
 
-  Botao ({ este .cor, este .texto});
+  Botao({this.cor, this.texto});
 
 }
 
-class  Pessoa {
+class Pessoa {
 
-  // atributos:
-  Nome da string ;
+  //atributos:
+  String nome;
   String email;
   int idade;
   bool fumante;
 
-  // Como "era antigamente" ...
-// Pessoa (String nome, String email, int idade, bool fumante) {
-// this.nome = nome;
-// this.email = email;
-// this.idade = idade;
-// this.fumante = fumante;
-//}
+  //Como "era antigamente"...
+//   Pessoa(String nome, String email, int idade, bool fumante) {
+//     this.nome = nome;
+//     this.email = email;
+//     this.idade = idade;
+//     this.fumante = fumante;
+//   }
 
-  // como usaremos, de forma menos verbosa:
-  Pessoa ( este .nome, este .email, este .idade, este .fumante);
+  //como usaremos, de forma menos verbosa:
+  Pessoa(this.nome, this.email, this.idade, this.fumante);
 
 
-  void  fazerAniversario () {
-    idade ++ ; // ++ é a mesma coisa que idade = idade + 1
-    print ( "ôba, festinha com todos mascarados" );
+  void fazerAniversario() {
+    idade++; //++ é a mesma coisa que idade = idade + 1
+    print("ôba, festinha com todos mascarados");
   }
 
-  String  falarEmail () {
-    retornar  "Meu e-mail é $ { email } " ;
+  String falarEmail() {
+    return "Meu e-mail é ${email}";
   }
 
-  void  comer ( String comida) {
-    print ( "Hmmm, adoro comer $ { comida } " );
+  void comer(String comida) {
+    print("Hmmm, adoro comer ${comida}");
   }
 }
 
 
-void  main () {
-  Pessoa p =  Pessoa ( "Thiago Traue" , "thiago.traue@uni9.pro.br" , 34 , falso );
+void main() {
+  Pessoa p = Pessoa("Thiago Traue", "danielrocha92@uni9.edu.br", 35, false);
 
-// p.nome = "Thiago Traue";
-// p.idade = 34;
-// p.email = "thiago.traue@uni9.pro.br";
-// p.fumante = true;
+//   p.nome = "Daniel Rocha";
+//   p.idade = 35;
+//   p.email = "danielrocha92@uni9.edu.br";
+//   p.fumante = false;
 
-  print ( "Olá $ { p . nome } , tudo bem?" );
-  print ( "Ah! Vi que você tem $ { p . idade } anos. Da hora!" );
+  print("Olá ${p.nome}, tudo bem?");
+  print("Ah! Vi que você tem ${p.idade} anos. Da hora!");
 
-  p. fazerAniversario ();
+  p.fazerAniversario();
 
   //print(p.idade);
 
-  imprimir (p. falarEmail ());
+  print(p.falarEmail());
 
-  // print (p);
+  //print(p);
 
-  p. comer ( "nutella" );
+  p.comer("nutella");
 
 
-  // só um exemplo:
-  Botao bt =  Botao (
-    texto :  "Oi" ,
-    cor :  "Verde"
+  //só um exemplo:
+  Botao bt = Botao(
+    texto: "Oi",
+    cor: "Verde"
   );
 }
