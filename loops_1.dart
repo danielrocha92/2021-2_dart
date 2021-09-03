732a6f17e96062f9734a04709c23f5fcdd835d4c
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 13:19:16 -0300

    Update loops_1.dart

--- loops_1.dart
+++ loops_1.dart
@@ -1,47 +1,47 @@
-class  Pessoa {
-  Nome da string ;
+class Pessoa {
+  String nome;
   int idade;
 
-  Pessoa ({ este .nome, este .idade});
+  Pessoa({this.nome, this.idade});
 }
 
-void  main () {
-  Listar < Pessoa > pessoas = [];
+void main() {
+  List<Pessoa> pessoas = [];
 
-  pessoas. adicionar (
-    Pessoa (
-      nome :  "Fulano" ,
-      idade :  20
+  pessoas.add(
+    Pessoa(
+      nome: "Fulano",
+      idade: 20
     )
   );
 
-  pessoas. adicionar (
-    Pessoa (
-      nome :  "Allisson" ,
-      idade :  18
+  pessoas.add(
+    Pessoa(
+      nome: "Allisson",
+      idade: 18
     )
   );
 
 
-  // laço para:
-// para (int i = 0; i <pessoas.length; i ++) {
-// print ("Nome: $ {pessoas [i] .nome}, $ {pessoas [i] .idade} anos");
-//}
+  //laço for:
+//   for(int i = 0; i < pessoas.length; i++) {
+//     print("Nome: ${pessoas[i].nome}, ${pessoas[i].idade} anos");
+//   }
 
-// int x = 0;
-// enquanto (x <pessoas.length) {
-// print ("Nome: $ {pessoas [x] .nome}, $ {pessoas [x] .idade} anos");
-// x ++;
-//}
+//   int x = 0;
+//   while(x < pessoas.length) {
+//     print("Nome: ${pessoas[x].nome}, ${pessoas[x].idade} anos");
+//     x++;
+//   }
 
-// int j = 0;
-// Faz {
-// print ("Nome: $ {pessoas [j] .nome}, $ {pessoas [j] .idade} anos");
-// j ++;
-//} while (j <pessoas.length);
+//   int j = 0;
+//   do {
+//     print("Nome: ${pessoas[j].nome}, ${pessoas[j].idade} anos");
+//     j++;
+//   } while(j < pessoas.length);
 
-  // para cada um (para cada)
-  para ( Pessoa p em pessoas) {
-    print ( "Nome: $ { p . nome } , $ { p . idade } anos" );
+  //for each (para cada)
+  for(Pessoa p in pessoas) {
+    print("Nome: ${p.nome}, ${p.idade} anos");
   }
 }
