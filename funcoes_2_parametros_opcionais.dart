2cd26019940362d09f9b40205dc3df5862b7ff42
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 13:09:13 -0300

    Update funcoes_2_parametros_opcionais.dart

--- funcoes_2_parametros_opcionais.dart
+++ funcoes_2_parametros_opcionais.dart
@@ -1,13 +1,13 @@
-void  main () {
-  criarBotao (
-    texto :  "Vai" ,
-    altura :  100
+void main() {
+  criarBotao(
+    texto: "Vai",
+    altura: 100
   );
 }
 
-void  criarBotao ({ String texto, String cor, double largura, double altura}) {
-  print ( "Criei um botão assim:" );
-  print ( "Texto: $ texto " );
-  print ( "Cor: $ { cor ?? " Cinza " } " );
-  print ( "Tamanho: $ { largura ?? 20 } por $ { altura ?? 5 } " );
+void criarBotao({String texto, String cor, double largura, double altura}) {
+  print("Criei um botão assim: ");
+  print("Texto: $texto");
+  print("Cor: ${cor ?? "Cinza"}");
+  print("Tamanho: ${largura ?? 20} por ${altura ?? 5}");
 }
