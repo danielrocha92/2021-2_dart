2a14cbdb5e8d6799fb5a61d8d0e8aba581e04692
Daniel Rocha <63888557+danielrocha92@users.noreply.github.com>
2021-09-03 13:16:43 -0300

    Update listas_1.dart

--- listas_1.dart
+++ listas_1.dart
@@ -1,24 +1,24 @@
-void  main () {
+void main() {
 
 
-  // Vetor é uma lista de itens do mesmo tipo classificadas (posições)
-  // são numeradas de 0 até o "tamanhho menos 1", ou seja
-  // um vetor de tamanho 5, pode ser acessado da posição 0 a 4
+  //Vetor é uma lista de itens do mesmo tipo cujos índices (posições)
+  //são numeradas de 0 até o "tamanhho menos 1", ou seja
+  //um vetor de tamanho 5, pode ser acessado da posição 0 a 4
 
-  Lista < String > compras = [ "Banana" , "Leite" , "Ovos" , "Nutella" ];
+  List<String> compras = ["Banana", "Leite", "Ovos", "Nutella"];
 
-  // adiciona, ao final da lista, um novo item
-  compras. adicionar ( "Abobrinha" );
+  //adiciona, ao final da lista, um novo item
+  compras.add("Abobrinha");
 
-  // imprime no console uma posição na lista
-  imprimir (compras [ 4 ]);
+  //imprime no console uma posição na lista
+  print(compras[4]);
 
-  // procura um item na lista (diferencia maiúsculas de minúsculas):
-  imprimir (compras. contém ( "banana" ));
+  //procura un item na lista (é case sensitive):
+  print(compras.contains("banana"));
 
-  // remove da lista um item:
-  compras. remover ( "Leite" );
+  //remove da lista um item:
+  compras.remove("Leite");
 
-  // imprime a lista inteira:
-  imprimir (compras);
+  //imprime a lista inteira:
+  print(compras);
 }
